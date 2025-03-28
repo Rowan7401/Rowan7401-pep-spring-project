32c349d39455af7fe5326367133e88378b2a786b
Rowan Dillon <rowan.j.dillon@gmail.com>
2025-03-28 22:15:42 +0000

    empty response body for deleteMessage?

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -108,7 +108,7 @@ public class SocialMediaController {
         }
         else {
             System.out.println("No message with this ID found to delete. Please try again.");
-            return ResponseEntity.status(200).body(deleted);
+            return ResponseEntity.status(200).body(null);
 
         }
        
